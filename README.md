11bec66002b9f0bc41a51aed2dc9fa1e00991ad6
Abu Hurayra <hurayraiit@gmail.com>
2022-10-02 19:17:36 +0600

    Ready GraphQL Server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gql-api-server
 A simple GraphQl API server built with NodeJS
+
+I am following [this tutorial](https://www.digitalocean.com/community/tutorials/how-to-set-up-a-graphql-api-server-in-node-js) from DigitalOcean.
---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express'
+import cors from 'cors'
+import { graphqlHTTP } from 'express-graphql'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+
+const app = express()
+const PORT = 4009
+
+// In-memory data store
+const data = {
+  warriors: [
+    { id: '001', name: 'Jaime' },
+    { id: '002', name: 'Jorah' },
+  ],
+}
+
+// Schema
+const typeDefs = `
+type Warrior {
+  id: ID!
+  name: String!
+}
+
+type Query {
+  warriors: [Warrior]
+}
+`
+
+
+
+// Resolver for warriors
+const resolvers = {
+    Query: {
+      warriors: (obj, args, context) => context.warriors,
+    },
+}
+  
+const executableSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Entrypoint
+app.use('/graphql', graphqlHTTP({
+    schema: executableSchema,
+    context: data,
+    graphiql: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Running a server at: http://localhost:${PORT}/graphql`);
+});
